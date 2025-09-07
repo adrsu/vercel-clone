d11afe52e840557f85d1dcdc4f45eae515ecc33b
Adarsh Sahu <adarshsahu1077@gmail.com>
2025-09-08 05:25:27 +0530

    Added simple-git clone

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,20 +10,24 @@ const git = simpleGit();
 app.use(cors());
 app.use(express.json());
 
-app.get("/repo", (req: Request, res: Response) => {
+app.get("/repo", async (req: Request, res: Response) => {
   const { repoUrl } = req.body;
 
   if (!repoUrl) {
     console.log("repo url not found: ", repoUrl);
+    return res.status(400).json({error: "repo url required"})
   }
 
   let idd = generate()
-  console.log(__dirname)
-  // git.clone(repoUrl, `out/${idd}`)
-  
 
-  res.send({"repoUrl": repoUrl, "idd": idd});
+  try {
+    await git.clone(repoUrl, `cloned_repo/${idd}`);
+  } catch (err) {
+    console.log('error: ', err);
+    return res.status(500).json({error: "clone failed"})
+  }
   
+  res.send({"repoUrl": repoUrl, "idd": idd});
 });
 
 app.listen(PORT, () => {
