54d08f1e00b4e1ec13cfd174ef15aee4230f9dce
Adarsh Sahu <adarshsahu1077@gmail.com>
2025-09-14 19:09:47 +0530

    redis queue pull logic

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello from index.tssss")
+
+import Redis from 'ioredis';
+
+const redis = new Redis();
+
+const processQueue = async () => {
+    while (1) {
+        const repo_id = await redis.blpop("my-queue",2);
+        console.log("report id:", repo_id);
+    }
+}
+
+processQueue();
