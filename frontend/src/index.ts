42d4a903168d032bee380f3babc48f5450e61b9c
Adarsh Sahu <adarshsahu1077@gmail.com>
2025-09-16 00:50:53 +0530

    Deploy Service

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -4,26 +4,22 @@ const Redis = require('ioredis');
 const redis = new Redis();
 
 const processQueue = async () => {
-    // while (1) {
-    //     const result = await redis.blpop("my-queue",5);
-    //     if (result) {
-    //         try {
-    //             const bucketName = result?.[1] || null
-    //             await downloadDirectoryWithStreaming('vercel-clone-s3bucket', bucketName, `./dist/downloads/${bucketName}`);
-    //             await buildUploadToS3(bucketName);
-    //         } catch (err) {
-    //             console.log("bucket: ", result?.[1])
-    //             console.log("error downloading: ", err);
-    //         }
-    //     }
+    while (1) {
+        const result = await redis.blpop("my-queue",5);
+        if (result) {
+            try {
+                const bucketName = result?.[1] || null
+                await downloadDirectoryWithStreaming('vercel-clone-s3bucket', bucketName, `./dist/downloads/${bucketName}`);
+                await buildUploadToS3(bucketName);
+                await redis.set(`status:${bucketName}`, "processed");
+            } catch (err) {
+                console.log("bucket: ", result?.[1])
+                console.log("error downloading: ", err);
+            }
+        }
         
-    //     console.log("repository:", result);
+        console.log("repository:", result);
         
-    // }
-    try {
-        console.log("building", await buildUploadToS3('ypsbm'));
-    } catch (err) {
-        console.log("errr0r", err);
     }
 }
 
