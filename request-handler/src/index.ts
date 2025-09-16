17c5df981c141dc495f9bf13fc3cfa87d20a88b2
Adarsh Sahu <adarshsahu1077@gmail.com>
2025-09-17 00:09:41 +0530

    request handler service

--- request-handler/src/index.ts
+++ request-handler/src/index.ts
@@ -1,21 +1,64 @@
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import cors from 'cors';
 import express, { Request, Response } from "express";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const PORT = 3001;
 
-app.use(cors());
+app.use(cors());    
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    // const { host } = req.body;
-    const host = req.hostname;
-    const idd = host.split(".")[0]
-    console.log("host", host);
-    console.log("iddd", idd);
-    res.send({"iddd": idd, "urll":host})
-
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION || "us-east-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY || '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+    }
 });
+
+const handleS3Request = async (req: Request, res: Response, requestedPath: string) => {
+    const id = req.hostname.split(".")[0];
+    
+    console.log("Project ID:", id);
+    console.log("Requested path:", requestedPath);
+    console.log("S3 Key:", `${id}/dist${requestedPath}`);
+    
+    const getCommand = new GetObjectCommand({
+        Bucket: 'vercel-clone-s3bucket',
+        Key: `${id}/dist${requestedPath}`
+    });
+    
+    const getResponse = await s3Client.send(getCommand);
+    
+    const content = await getResponse.Body?.transformToString();
+    
+    const contentType = requestedPath.endsWith('html')? 'text/html': requestedPath.endsWith('css')? 'text/css': requestedPath.endsWith('js')? 'application/javascript': 'text/plain';
+    res.set('Content-Type', contentType);
+    
+    res.send(content);
+}
+
+app.get('/', async (req: Request, res: Response) => {
+    await handleS3Request(req, res, '/index.html');
+});
+
+app.get('/assets/:filename', async (req: Request, res: Response) => {
+    const filename = req.params.filename;
+    await handleS3Request(req, res, `/assets/${filename}`);
+});
+
+app.get('/:filename.:ext', async (req: Request, res: Response) => {
+    const filename = req.params.filename;
+    const ext = req.params.ext;
+    await handleS3Request(req, res, `/${filename}.${ext}`);
+});
+
+app.get('/:path', async (req: Request, res: Response) => {
+    const path = req.params.path;
+    await handleS3Request(req, res, `/${path}`);
+})
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
